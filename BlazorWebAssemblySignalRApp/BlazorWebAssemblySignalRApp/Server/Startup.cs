6c4d48a524378411dabb8328679c642ceb62e9c7
UchiTesing <uchitesting57@gmail.com>
2021-05-06 11:22:05 +0200

    Added services and endpoint for SignalR on the server project.

--- BlazorWebAssemblySignalRApp/BlazorWebAssemblySignalRApp/Server/Startup.cs
+++ BlazorWebAssemblySignalRApp/BlazorWebAssemblySignalRApp/Server/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.Extensions.Hosting;
 
 using System.Linq;
 
+using BlazorWebAssemblySignalRApp.Server.Hubs;
+
 namespace BlazorWebAssemblySignalRApp.Server
 {
    public class Startup
@@ -23,14 +25,20 @@ namespace BlazorWebAssemblySignalRApp.Server
       // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
       public void ConfigureServices(IServiceCollection services)
       {
-
+         services.AddSignalR();
          services.AddControllersWithViews();
          services.AddRazorPages();
+         services.AddResponseCompression(opts =>
+         {
+            opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(new[] { "application/octet-stream" });
+         });
       }
 
       // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
       public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
       {
+         app.UseResponseCompression();
+
          if (env.IsDevelopment())
          {
             app.UseDeveloperExceptionPage();
@@ -53,6 +61,7 @@ namespace BlazorWebAssemblySignalRApp.Server
          {
             endpoints.MapRazorPages();
             endpoints.MapControllers();
+            endpoints.MapHub<ChatHub>("/chathub");
             endpoints.MapFallbackToFile("index.html");
          });
       }
